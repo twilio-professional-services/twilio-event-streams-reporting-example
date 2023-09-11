36fa7089c45d8da81e391ce3010129b62a306c87
Sam Johnson <sam@scj.me>
2023-09-11 17:36:10 -0500

    Implement request validation

--- middleware/authenticate-twilio-signature.js
+++ middleware/authenticate-twilio-signature.js
@@ -1,7 +1,14 @@
-const twilio = require("twilio");
+const client = require("twilio");
 var debug = require("debug")("event-streams-backend:authentication");
 
 module.exports = (req, res, next) => {
-	debug("passed");
-	next();
+	const result = client.validateRequest(process.env.TWILIO_AUTH_TOKEN, req.get("X-Twilio-Signature"), `https://${req.get('host')}${req.originalUrl}`, {});
+	
+	if (result) {
+		debug("Request validation successful");
+		return next();
+	}
+	
+	debug("Request validation failed");
+	return next("Error");
 }
