0177232fc2c88039a8a2b455aa60d9de0021f9ff
Sam Johnson <sam@scj.me>
2023-09-11 17:37:23 -0500

    Tabs to spaces

--- middleware/authenticate-twilio-signature.js
+++ middleware/authenticate-twilio-signature.js
@@ -2,13 +2,13 @@ const client = require("twilio");
 var debug = require("debug")("event-streams-backend:authentication");
 
 module.exports = (req, res, next) => {
-	const result = client.validateRequest(process.env.TWILIO_AUTH_TOKEN, req.get("X-Twilio-Signature"), `https://${req.get('host')}${req.originalUrl}`, {});
-	
-	if (result) {
-		debug("Request validation successful");
-		return next();
-	}
-	
-	debug("Request validation failed");
-	return next("Error");
+  const result = client.validateRequest(process.env.TWILIO_AUTH_TOKEN, req.get("X-Twilio-Signature"), `https://${req.get('host')}${req.originalUrl}`, {});
+  
+  if (result) {
+    debug("Request validation successful");
+    return next();
+  }
+  
+  debug("Request validation failed");
+  return next("Error");
 }
